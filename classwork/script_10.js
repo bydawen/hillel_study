04987d8211913bd30a129e98ea215fa97bd8459c
oleksandr.buhaienko <oleksandr.buhaienko@raccoongang.com>
2025-07-31 09:21:07 +0300

    feat: add code for classwork (6-10)

---
+++ classwork/script_10.js
@@ -0,0 +1,7 @@
+for (let i = 1; i <= 10; i++) {
+  for (let j = 1; j <= 10; j++) {
+    let multi = `${i} * ${j} = ${i * j} <br>`;
+    document.write(multi);
+  }
+  document.write('<br>');
+}
---
+++ classwork/script_6.js
@@ -0,0 +1,8 @@
+let startNumber = 0;
+
+for (let i = 1; i <= 500; i++) {
+  startNumber = startNumber + i;
+}
+
+let average = startNumber / 500;
+console.log(average);
