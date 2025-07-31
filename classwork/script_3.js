774b15adaf58f7d6559993bfe057fca1d4bd6ff8
oleksandr.buhaienko <oleksandr.buhaienko@raccoongang.com>
2025-07-31 16:01:25 +0300

    feat: add comments for classwork scripts

--- classwork/script_3.js
+++ classwork/script_3.js
@@ -1,5 +1,7 @@
+// Вивести таблицю множення на 7 в форматі (1*7 = 7; 2*7 = 14…)
+
 let counter = 7;
 
 for (let i = 1; i <= 7; i++) {
-  console.log(`${i} * ${counter} = ${i * counter};`);
+  console.log(`${i} * ${counter} = ${i * counter}`);
 }
