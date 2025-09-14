dff571f381cf281440231fa2896f13fd1b35b1bb
oleksandr.buhaienko <oleksandr.buhaienko@raccoongang.com>
2025-09-14 12:01:49 +0300

    feat: code for homework 14_1

---
+++ homework/14_1/scripts.js
@@ -0,0 +1,81 @@
+// Пишемо свій слайдер зображень, який повинен:
+// Відображати зображення та кнопки Next, Prev з боків від зображення.
+// При кліку на Next - показуємо наступне зображення.
+// При кліку на Prev - попереднє
+// При досягненні останнього зображення - ховати кнопку Next. Аналогічно з першим зображенням і кнопкою Prev
+// Кількість слайдів може бути будь-якою
+// Додати можливість навігації через точки під слайдами
+
+const images = [
+  './images/1.jpg',
+  './images/2.png',
+  './images/3.jpg',
+  './images/4.jpg',
+  './images/5.jpg',
+  './images/6.jpg',
+  './images/7.jpg',
+  './images/8.jpg',
+  './images/9.jpg',
+  './images/10.jpg',
+];
+
+let sliderItem = document.querySelector('.slider-item');
+let currentSlide = 0;
+
+let sliderImg = document.createElement('img');
+sliderImg.src = images[currentSlide];
+
+sliderItem.appendChild(sliderImg);
+
+let nextSlideBtn = document.getElementById('imageNext');
+nextSlideBtn.addEventListener('click', () => {
+  currentSlide += 1;
+
+  if (currentSlide >= images.length) {
+    currentSlide = 0;
+  }
+
+  sliderImg.src = images[currentSlide];
+  setSliderActiveDot();
+});
+
+let prevSlideBtn = document.getElementById('imagePrev');
+prevSlideBtn.addEventListener('click', () => {
+  currentSlide -= 1;
+
+  if (currentSlide < 0) {
+    currentSlide = images.length - 1;
+  }
+
+  sliderImg.src = images[currentSlide];
+  setSliderActiveDot();
+});
+
+let sliderDots = document.querySelector('.slider-nav-dots');
+
+generateSliderDots = () => {
+  images.forEach((image, index) => {
+    let sliderDotItem = document.createElement('li');
+    sliderDotItem.classList.add('slider-dot');
+
+    if (index === currentSlide) {
+      sliderDotItem.classList.add('is-active');
+    }
+
+    sliderDotItem.addEventListener('click', () => {
+      sliderImg.src = images[index];
+
+      setSliderActiveDot();
+    });
+
+    sliderDots.appendChild(sliderDotItem);
+  });
+};
+
+generateSliderDots();
+
+setSliderActiveDot = () => {
+  document.querySelectorAll('.slider-dot').forEach((item, index) => {
+    item.classList.toggle('is-active', index === currentSlide);
+  });
+};
